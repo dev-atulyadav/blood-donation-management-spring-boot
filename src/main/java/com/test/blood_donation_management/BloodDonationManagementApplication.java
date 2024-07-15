311f02290f8f02df63267dccee56979f588f93db
dev-atulyadav <atulyadav031103@gmail.com>
2024-07-15 23:59:14 +0530

    initial commit

---
+++ src/main/java/com/test/blood_donation_management/BloodDonationManagementApplication.java
@@ -0,0 +1,13 @@
+package com.test.blood_donation_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodDonationManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodDonationManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/blood_donation_management/BloodDonationManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.blood_donation_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodDonationManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
